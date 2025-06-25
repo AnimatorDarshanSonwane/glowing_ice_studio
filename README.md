5a52f8d7dfd7297ab43f7fdfdaa282b429762e9f
Darshan Sharad Sonwane <darshansonwane00000@gmail.com>
2025-06-25 12:58:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# glowing_ice_studio
+Gaming and Art Services
---
+++ android/app/src/main/kotlin/com/example/glowing_ice_studio/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.glowing_ice_studio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
